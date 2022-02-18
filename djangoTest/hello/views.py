f69e659d4d1825a2c9b9db5a07cc35f7d9ef4993
황해도 <goehehgo@naver.com>
2022-02-18 10:43:44 +0900

    수정

--- djangoTest/hello/views.py
+++ djangoTest/hello/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(reques):
-    return HttpResponse("Hello, world. You're at the hello index. 안녕하세요 ㅎㅎ")
+    return HttpResponse("Hello, world. You're at the hello index. 안녕하세요 ㅎㅎ22")
